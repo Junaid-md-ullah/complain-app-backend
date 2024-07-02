8df090152f7b47c6d6e5d8175433027d792b743c
JunaidSe <junaid@echologyx.com>
2024-07-02 22:14:10 +0600

    basic get and post done

--- server.js
+++ server.js
@@ -5,33 +5,64 @@ import path from "path";
 import fs from "fs/promises";
 const PORT = process.env.PORT;
 
+const all_comlains = [
+  {
+    id: 1,
+    comment: "This is what it is",
+    rating: 4,
+  },
+  {
+    id: 1,
+    comment: "This is what it is",
+    rating: 4,
+  },
+];
+
+//JSON middleware
+const jsonMiddleware = (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+};
+// ROute handler for GET /api/users
+const getUserHandler = (req, res) => {
+  res.write(JSON.stringify(all_comlains));
+  res.end();
+};
+const creatUserHandler = (req, res) => {
+  let body = "";
+  //Listen for data
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    const newComplain = JSON.parse(body);
+    all_comlains.push(newComplain);
+    res.statusCode = 201;
+    res.write(JSON.stringify(newComplain));
+    res.end();
+  });
+};
+// NOt found handler
+const notFoundHandler = (req, res) => {
+  res.statusCode = 404;
+  res.write(JSON.stringify({ message: "Message not found" }));
+  res.end();
+};
+
 // Get current path
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = http.createServer(async (req, res) => {
-  try {
-    // Check if GET request
-    if (req.method === "GET") {
-      let filePath;
-      if (req.url === "/") {
-        filePath = path.join(__dirname, "public", "index.html");
-      } else if (req.url === "/about") {
-        filePath = path.join(__dirname, "public", "about.html");
-      } else {
-        throw new Error("Not Found");
-      }
-      const data = await fs.readFile(filePath);
-      res.setHeader("Content-Type", "text/html");
-      res.write(data);
-      res.end();
+  jsonMiddleware(req, res, () => {
+    if (req.url === "/api/allComplains" && req.method === "GET") {
+      getUserHandler(req, res);
+    } else if (req.url === "/api/addComplain" && req.method === "POST") {
+      creatUserHandler(req, res);
     } else {
-      throw new Error("Method not allowed");
+      notFoundHandler(req, res);
     }
-  } catch (error) {
-    res.writeHead(500, { "Content-Type": "text/plain" });
-    res.end("Server Error");
-  }
+  });
 });
 
 server.listen(8000, () => {
